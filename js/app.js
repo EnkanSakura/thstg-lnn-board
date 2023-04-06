265f5338eaaa433c90b513b988788d0615d8139e
EnkanSakura <759572692@qq.com>
2023-04-06 19:33:33 +0800

    表加上去了，但没改完

--- js/app.js
+++ js/app.js
@@ -16,21 +16,38 @@ fetch(`data/${tableData}`)
   .then(data => {
     // 查找表格元素
     const table = document.querySelector('tbody');
-
+    // 按 ID 排序
+    let keySort = Object.keys(data);
+    // let keySort = Object.keys(data).sort(function (a, b) {
+    //   const aid = a.replace(' ', '').split('-');
+    //   const bid = b.replace(' ', '').split('-');
+    //   if (aid[0].localeCompare(bid[0]) === 0) {
+    //     return aid[1].localeCompare(bid[1]);
+    //   } else {
+    //     return aid[0].localeCompare(bid[0]);
+    //   }
+    // });
     // 渲染表格数据
-    for (let id in data) {
+    keySort.forEach(id => {
       const userData = data[id];
+      let firstLine = true;
       userData.forEach(recordData => {
-        console.log(recordData);
         const row = document.createElement('tr');
-        const idCell = document.createElement('td');
-        idCell.textContent = id;
-        row.appendChild(idCell);
+        if (firstLine) {
+          // 第一行添加ID, 合并行
+          const idCell = document.createElement('td');
+          idCell.rowSpan = userData.length;
+          idCell.textContent = id;
+          row.appendChild(idCell);
+          firstLine = false;
+        }
         for (let key in recordData) {
-          console.log(key, recordData[key]);
           const cell = document.createElement('td');
           if (boolKeyList.indexOf(key) !== -1) {
-            if (typeof(recordData[key]) === 'boolean') {
+            if (recordData[key] === '' || recordData[key] === null) {
+              cell.textContent = '-';
+            }
+            else if (typeof(recordData[key]) === 'boolean') {
               cell.textContent = recordData[key] ? '√' : '×';
             }
             else {
@@ -49,7 +66,7 @@ fetch(`data/${tableData}`)
         }
         table.appendChild(row);
       });
-    }
+    });
   }
 );
 
