725ddf042a5ec7702ef549b74c21ec9c02ba0730
EnkanSakura <759572692@qq.com>
2023-04-11 19:44:39 +0800

    悬浮窗信息展示

--- js/interact.js
+++ js/interact.js
@@ -1,4 +1,57 @@
 // 返回键
 function goBack() {
-    location.assign(location.href.split('/').slice(0, -1).join('/'));
+  location.assign(location.href.split('/').slice(0, -1).join('/'));
+}
+
+// 悬浮窗
+class Suspended {
+  constructor(targetId, content) {
+    this.target = document.getElementById(targetId);
+    this.content = content;
+    this.suspended = null;
+    this.mask = null;
+    this.active = false;
+    this.init();
+  }
+
+  init () {
+    const container = document.getElementsByClassName('container')[0];
+    this.suspended = document.createElement('div');
+    this.suspended.classList.add('suspended');
+    this.suspended.classList.add('inactive');
+    // this.suspended.innerHTML = this.html;
+    fetch(this.content)
+      .then(res => res.text())
+      .then(html => {
+        this.suspended.innerHTML = html;
+      });
+
+    container.appendChild(this.suspended);
+    this.mask = document.createElement('div');
+    this.mask.classList.add('mask');
+    container.appendChild(this.mask);
+
+    this.target.addEventListener('click', this.show.bind(this));
+    this.mask.addEventListener('click', this.hide.bind(this));
+  }
+
+  show () {
+    if (this.active) return;
+
+    this.mask.classList.add('active');
+    this.suspended.classList.add('active');
+    this.active = true;
+    this.mask.classList.remove('inactive');
+    this.suspended.classList.remove('inactive');
+  }
+
+  hide () {
+    if (!this.active) return;
+
+    this.mask.classList.add('inactive');
+    this.suspended.classList.add('inactive');
+    this.active = false;
+    this.mask.classList.remove('active');
+    this.suspended.classList.remove('active');
+  }
 }
