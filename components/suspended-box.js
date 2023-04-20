068854884e8118efa3968cca98cd1b7eeffdad54
EnkanSakura <759572692@qq.com>
2023-04-20 12:31:20 +0800

    fix suspended

--- components/suspended-box.js
+++ components/suspended-box.js
@@ -3,7 +3,7 @@ class SuspendedBox {
   constructor(targetId, content) {
     this.target = document.getElementById(targetId);
     this.content = content;
-    this.suspended-box = null;
+    this.suspended = null;
     this.mask = null;
     this.active = false;
     this.init();
@@ -11,16 +11,16 @@ class SuspendedBox {
 
   init() {
     const container = document.getElementsByClassName('container')[0];
-    this.suspended-box = document.createElement('div');
-    this.suspended-box.classList.add('suspended-box');
-    this.suspended-box.classList.add('inactive');
+    this.suspended = document.createElement('div');
+    this.suspended.classList.add('suspended');
+    this.suspended.classList.add('inactive');
     fetch(`./html/${this.content}.html`)
       .then(response => response.text())
       .then(data => {
-        this.suspended-box.innerHTML = data;
+        this.suspended.innerHTML = data;
       });
 
-    container.appendChild(this.suspended-box);
+    container.appendChild(this.suspended);
     this.mask = document.createElement('div');
     this.mask.classList.add('mask');
     container.appendChild(this.mask);
@@ -33,19 +33,19 @@ class SuspendedBox {
     if (this.active) return;
 
     this.mask.classList.add('active');
-    this.suspended-box.classList.add('active');
+    this.suspended.classList.add('active');
     this.active = true;
     this.mask.classList.remove('inactive');
-    this.suspended-box.classList.remove('inactive');
+    this.suspended.classList.remove('inactive');
   }
 
   hide() {
     if (!this.active) return;
 
     this.mask.classList.add('inactive');
-    this.suspended-box.classList.add('inactive');
+    this.suspended.classList.add('inactive');
     this.active = false;
     this.mask.classList.remove('active');
-    this.suspended-box.classList.remove('active');
+    this.suspended.classList.remove('active');
   }
 }
